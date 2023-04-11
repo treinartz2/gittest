8d98a07f9c25558e62fcebd951ddf09bf9c5306e
treinartz2 <thomas.reinartz2@gmail.com>
2023-04-11 18:59:40 -0500

    Add files via upload

---
+++ week1part1.py
@@ -0,0 +1,45 @@
+## LastName  Week1   Step1
+
+
+#From Runestone:
+
+#1 Expressions
+
+#3 24 hour clock
+
+#5 All work and no play makes Jack a dull boy.
+
+#7 compound interest
+
+#9 area of a rectangle
+
+#11 convert degrees celsius to degrees fahrenheit.
+
+## From Homework1 pdf
+
+#1weight_on_mars
+def fun(x):
+    if x >= 1:
+        print(x, end=" ")
+        fun(x - 1)
+        
+fun(3)
+
+#2pizza_price
+
+#3summer_trip
+
+
+
+#4cycle_shop
+
+
+
+#5area_of_circle
+
+
+#6eggs_in-carton
+
+
+
+#7change_machine!
